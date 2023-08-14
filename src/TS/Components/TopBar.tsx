dcfd716b35f4318d710f62bb98b6c2e6385349e9
sonny <sonnymparker9@gmail.com>
2023-08-14 14:17:03 +0100

    Name change.
    Description.

--- src/TS/Components/TopBar.tsx
+++ src/TS/Components/TopBar.tsx
@@ -1,13 +1,15 @@
 import "../../CSS/TopBar.css"
-import GitHubIcon from '@mui/icons-material/GitHub';
-function TopBar () {
+
+function TopBar() {
     return (
         <div id={"top-bar-wrapper"}>
             <div id={'top-bar-main'}>
-                <h1>robots.txt testing</h1>
-            </div>
-            <div className={'top-bar-anchors-wrapper'}>
-                <a className={'top-bar-anchor'} href={'https://github.com/Xvois/robots-validator'}><GitHubIcon fontSize={'small'} />Github</a>
+                <h1>robots.txt Validator and Testing Tool</h1>
+                <h2>What is a robots.txt file?</h2>
+                <p>The robots.txt file is a simple text file used to tell Search Engines about the folders and files on
+                    the domain that may be crawled (looked at) by the search engine’s crawler, as well as those that
+                    cannot/should not be seen.
+                    It is also good practice to include a link/reference to an XML sitemap in the robots.txt file.</p>
             </div>
         </div>
     )
