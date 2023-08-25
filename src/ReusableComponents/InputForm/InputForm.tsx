13077d67dcc4f7547c8f93fe55773609f6774723
Piers Parker <piers@view6.com>
2023-08-25 16:07:07 +0100

    manifest name update

--- src/ReusableComponents/InputForm/InputForm.tsx
+++ src/ReusableComponents/InputForm/InputForm.tsx
@@ -174,7 +174,7 @@ export function InputForm(
                                     <Select onValueChange={field.onChange} defaultValue={field.value}>
                                         <FormControl>
                                             <SelectTrigger>
-                                                <SelectValue placeholder="Select a target platform"/>
+                                                <SelectValue aria-label="Select a target platform" placeholder="Select a target platform"/>
                                             </SelectTrigger>
                                         </FormControl>
                                         <SelectContent>
