6bd312d2f21390e16edd3795ed04544779c4e1b9
Piers Parker <piers@view6.com>
2023-09-12 15:33:24 +0100

    content added to terms and privacy pages

--- src/Pages/Privacy.tsx
+++ src/Pages/Privacy.tsx
@@ -1,8 +1,60 @@
 export default function Privacy() {
     return (
         <div>
-            <h2>This is the Privacy Policy page.</h2>
-            <p>This is some text!</p>
+            <h2>Our Privacy Policy</h2>
+            <p>This policy sets out the different areas where user privacy is concerned and outlines the obligations & requirements of the users, the website and website owner. Furthermore the way this website processes, stores and protects user data and information will also be detailed within this policy.
+            The Website</p>
+
+            <p>This website and its owner take a proactive approach to user privacy and ensure the necessary steps are taken to protect the privacy of its users throughout their visiting experience.</p>
+
+            <h3>Access logs</h3>
+
+            <p>The hosting of the https://www.robots-validator.com/ website stores access logs. The information inside these log files includes internet protocol (IP) addresses of visitors of httpstatus.io, and is stored for less than 30 days. This data is not accessible to and not readable for the owner of this website.</p>
+
+            <h3>About cookies</h3>
+
+            <h4>Cookie consent</h4>
+            <p>This website uses cookies. By using this website and agreeing to this policy, you consent to https://www.robots-validator.com/'s use of cookies in accordance with the terms of this policy. Cookies are information packets sent by web servers to web browsers, and stored by the web browsers. The information is then sent back to the server each time the browser requests a page from the server. This enables a web server to identify and track web browsers. There are two main kinds of cookies: session cookies and persistent cookies. Session cookies are deleted from your computer when you close your browser, whereas persistent cookies remain stored on your computer until deleted, or until they reach their expiry date.</p>
+            <p>In order to comply with your preferences, https://www.robots-validator.com/ stores two functional cookies so that you're not asked to make this choice again, and to be able to save your settings. Please note that these cookies are only used to guarantee a user-friendly visit and a proper website performance.</p>
+
+            <h4>Refusing cookies</h4>
+            <p>If you wish to disable cookies, you may do so through your individual browser options. More detailed information about cookie management with specific web browsers can be found at the browsers' respective websites.</p>
+
+
+            <h3>About links</h3>
+
+            <h4>External links</h4>
+            <p>Although this website only looks to include quality, safe and relevant external links users should always adopt a policy of caution before clicking any external web links mentioned throughout this website. The owner of this website cannot guarantee or verify the contents of any externally linked website despite his best efforts. Users should therefore note they click on external links at their own risk and this website and its owner cannot be held liable for any damages or implications caused by visiting any external links mentioned.</p>
+
+            <h4>Ads and Sponsored Links</h4>
+            <p>This website may contain sponsored links and adverts. These will typically be served through advertising partners, to whom may have detailed privacy policies relating directly to the adverts they serve. Clicking on any such adverts will send you to the advertisers website through a referral program which may use cookies and will track the number of referrals sent from this website. This may include the use of cookies which may in turn be saved on your computer's hard drive. Users should therefore note they click on sponsored external links at their own risk and this website and its owner cannot be held liable for any damages or implications caused by visiting any external links mentioned.</p>
+
+            <h3>Social Media Platforms</h3>
+
+            <h4>Communication</h4>
+            <p>Communication, engagement and actions taken through external social media platforms that this website and its owner participate on are custom to the terms and conditions as well as the privacy policies held with each social media platform respectively. Users are advised to use social media platforms wisely and communicate/engage upon them with due care and caution in regard to their own privacy and personal details. This website nor its owner will ever ask for personal or sensitive information through social media platforms and encourage users wishing to discuss sensitive details to contact them through primary communication channels such as by telephone or email.</p>
+
+            <h4>Social sharing buttons</h4>
+            <p>This website may use social sharing buttons which help share web content directly from web pages to the social media platform in question. Users are advised before using such social sharing buttons that they do so at their own discretion and note that the social media platform may track and save your request to share a web page respectively through your social media platform account.</p>
+
+            <h3>Consent</h3>
+
+            <p>By continued use of this site, you consent to https://www.robots-validator.com/ storing and using cookies, and consent to this privacy policy and agree to its terms. If you do not consent, go to your browser settings to manage cookies or discontinue using this site.</p>
+
+            <h3>Credit</h3>
+
+            <p>Some parts of this document were created using a Contractology template available at http://www.freenetlaw.com and Free Website Privacy Policy written by Jamie King Media.</p>
+
+
+            <h3>Policy changes</h3>
+
+            <p>Changes in this policy will be posted on this page. You are advised to check this page regularly to view the most recent privacy policy.</p>
+
+
+            <h3>Contact Us</h3>
+
+            <p>If you have any questions about this Policy, you can contact us https://www.robots-validator.com/</p>
+
         </div>
     )
 }
